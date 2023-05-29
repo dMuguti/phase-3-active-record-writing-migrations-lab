0b6fd4121b16b3c9a89deb006c07a8a6d131523a
dMuguti <omiahderek56@gmail.com>
2023-05-29 23:40:56 +0300

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ActiveRecord::Base
 end
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  );
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
